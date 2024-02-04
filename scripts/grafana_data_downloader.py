9d75fadd9fe56f645e60d06a3491311df1b330ae
eugene.gleba <glebajk@gmail.com>
2024-02-04 15:59:46 +0300

    Small fixes to data downloader

--- scripts/grafana_data_downloader.py
+++ scripts/grafana_data_downloader.py
@@ -1,6 +1,4 @@
-import argparse
 import logging
-import threading
 from pathlib import Path
 from typing import List
 
@@ -13,9 +11,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
-
-# Create argument parser
-parser = argparse.ArgumentParser(description='Web scraping script')
 
 
 def create_driver(
@@ -34,25 +29,12 @@ def create_driver(
     return webdriver.Firefox(options=options)
 
 
-def execute_script_from_file(driver: webdriver, file_path: str) -> None:
-    """Execute a JavaScript script from a file."""
-    if not Path(file_path).is_file():
-        raise FileNotFoundError(file_path)
-
-    if not file_path.endswith('.js'):
-        raise RuntimeError(f"Given file is not js: {file_path}")
-
-    with open(file_path, 'r') as f:
-        script = f.read()
-
-    driver.execute_script(script)
-
-
 def get_clear_url(url: str) -> str:
     """Remove query parameters from a URL."""
     return url.split('?')[0]
 
 
+# Todo: make it download to ../data/<dir_name>
 def make_custom_download_dir_for_url_and_get_it(url: str) -> str:
     """Create a custom download directory for a URL and return its absolute path."""
     dir_name: str = url.split('/')[-1]
@@ -81,13 +63,11 @@ def process_elements(driver: webdriver) -> None:
     """Process elements in a webpage."""
     try:
         # Define the CSS selectors
-        div_selector = 'div[class^="drawer drawer-right drawer-open css-"]'
-        button_selector = 'button[class^="css-"][class$="-button"]'
+        div_selector: str = 'div[class^="drawer drawer-right drawer-open css-"]'
+        button_selector: str = 'button[class^="css-"][class$="-button"]'
 
-        # Find the div
         div = driver.find_element(By.CSS_SELECTOR, div_selector)
 
-        # Find the button within the div
         button = div.find_element(By.CSS_SELECTOR, button_selector)
 
         button.click()
@@ -95,24 +75,19 @@ def process_elements(driver: webdriver) -> None:
         print(f"An error occurred: {str(e)}")
 
 
-def read_urls_from_config(config_path: str) -> List[str]:
-    """Read URLs from a configuration file."""
-    with open(config_path, 'r') as f:
-        urls = [line.strip() for line in f]
-    return urls
-
-
-def process_url(url):
+def process_url(url: str):
     clear_url: str = get_clear_url(url)
+    print(f"clear_url: {clear_url}")
     download_path: str = make_custom_download_dir_for_url_and_get_it(clear_url)
     print(f"download_path: {download_path}")
     firefox_driver = create_driver(download_path)
-    print(f"clear_url: {clear_url}")
     firefox_driver.get(clear_url)
     panels_ids_list: List[int] = get_existing_panels(firefox_driver)
     print(f"panels_ids_list: {panels_ids_list}")
     for i in panels_ids_list:  # Loop from start to end
-        new_url = f"{clear_url}?orgId=1&from=now-5y&to=now&inspect={i}"
+        period_from: str = 'now'
+        period_to: str = '5y'
+        new_url = f"{clear_url}?orgId=1&from={period_from}-{period_to}&to=now&inspect={i}"
         firefox_driver.get(new_url)
         try:
             WebDriverWait(firefox_driver, 2).until(
@@ -127,11 +102,6 @@ def process_url(url):
 
 
 if __name__ == '__main__':
-    urls = read_urls_from_config('satellite_pages.txt')
-    threads = []
+    urls = ['https://dashboard.satnogs.org/d/uSu4Uxank/sanosat-1?orgId=1']
     for url in urls:
-        t = threading.Thread(target=process_url, args=(url,))
-        threads.append(t)
-        t.start()
-    for t in threads:
-        t.join()
+        process_url(url)
