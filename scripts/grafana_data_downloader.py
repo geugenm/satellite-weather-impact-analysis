b0411f2e98d26496ba32a16597deea20b80093a4
eugene.gleba <glebajk@gmail.com>
2024-04-07 16:50:50 +0300

    Small improvements to downloader scripts

--- scripts/grafana_data_downloader.py
+++ scripts/grafana_data_downloader.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import time
 from typing import List, Tuple
 
 from selenium import webdriver
@@ -9,13 +10,17 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+import os
+from urllib.parse import urlparse, ParseResult
+
+from tqdm import tqdm
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
 
 def create_driver(browser_download_dir: str,
                   headless: bool = True) -> webdriver:
-    """Create a Firefox webdriver with custom options."""
     options = Options()
     if headless:
         options.add_argument("--headless")  # Run in headless mode
@@ -29,38 +34,30 @@ def create_driver(browser_download_dir: str,
     return webdriver.Firefox(options=options)
 
 
-def get_clear_url(url: str) -> str:
-    """Remove query parameters from a URL."""
-    return url.split('?')[0]
-
-
-def make_custom_download_dir_for_url_and_get_it(url: str,
-                                                base_dir: str) -> str:
+def make_download_dir(url: str, base_dir: str) -> str:
     """Create a custom download directory for a URL and return its absolute path."""
-    dir_name = url.split('/')[-1]
-    logging.info(f"Downloading sat=[{dir_name}]")
-    full_path = Path(base_dir) / dir_name
-    full_path.mkdir(parents=True, exist_ok=True)
-    return str(full_path.resolve())
+    parsed_url: ParseResult = urlparse(url)
+    dir_name: str = os.path.basename(parsed_url.path)
+    download_path: str = os.path.join(base_dir, dir_name)
+    os.makedirs(download_path, exist_ok=True)
+    return download_path
 
 
 def get_existing_panels(driver: webdriver) -> List[Tuple[int, str]]:
-    """Get IDs and titles of existing panels."""
+    """Get IDs and titles of existing panels, handling potential exceptions."""
     existing_panels = []
-    for panel_id in range(
-        101):  # The range starts from 0, so 101 is needed to reach 100
-        element_id: str = f"panel-{panel_id}"
+    for panel_id in tqdm(range(100), desc="Identifying panels"):
         try:
-            element = driver.find_element(By.ID, element_id)
+            element = driver.find_element(By.ID, f"panel-{panel_id}")
             if 'react-grid-item' in element.get_attribute('class'):
-                scroll_to_element(driver, element_id)
-
-                title_element = element.find_element(By.CLASS_NAME,
-                                                     'panel-title-text')
-                title = title_element.text
-                existing_panels.append((panel_id, title))
+                title_element = element.find_element(By.CLASS_NAME, 'panel-title-text')
+                existing_panels.append((panel_id, title_element.text))
         except NoSuchElementException:
-            continue
+            pass  # Ignore panels that don't exist or don't match criteria
+
+    for panel_id, panel_name in existing_panels:
+        print(f"Panel ID: {panel_id}, Name: {panel_name}")
+
     return existing_panels
 
 
@@ -84,21 +81,30 @@ def scroll_to_element(driver: webdriver, css_selector: str) -> None:
 
 def process_url(url: str, download_dir: str, period_from: str = 'now',
                 period_to: str = '9y') -> None:
-    clear_url: str = get_clear_url(url)
-    download_path: str = make_custom_download_dir_for_url_and_get_it(clear_url,
-                                                                     download_dir)
+    clear_url: str = url.split('?')[0]
+    download_path: str = make_download_dir(clear_url,
+                                           download_dir)
+
+    logging.info("Creating browsing firefox_driver...")
     firefox_driver: webdriver = create_driver(download_path)
+    logging.info(f'Set download_directory=[{download_path}]')
+
     firefox_driver.get(clear_url)
+
+    logging.info("Waiting for page load. Press Ctrl+C to quit...")
+    time.sleep(10)
+
+    logging.info(f'Obtaining panels...')
     panels_ids_list: List[Tuple[int, str]] = get_existing_panels(firefox_driver)
 
     for panel_id, panel_name in panels_ids_list:  # Loop from start to end
-        new_url = f"{clear_url}?orgId=1&from={period_from}-{period_to}&to=now&inspect={panel_id}"
+        new_url: str = f"{clear_url}?orgId=1&from={period_from}-{period_to}&to=now&inspect={panel_id}"
         firefox_driver.get(new_url)
 
         try:
             element_present = EC.presence_of_element_located(
                 (By.ID, f"panel-{panel_id}"))
-            WebDriverWait(firefox_driver, 2).until(element_present)
+            WebDriverWait(firefox_driver, 10).until(element_present)
         except TimeoutException:
             logging.warning(f"timeout: page url=[{url}] is not loaded")
             continue
@@ -113,17 +119,17 @@ def process_url(url: str, download_dir: str, period_from: str = 'now',
                 element_present)
         except TimeoutException:
             logging.warning(
-                f"panel=[{panel_name}] timeout: download button is not found on url=[{new_url}]")
+                f"timeout: panel=[{panel_name}] download button is not found, url=[{new_url}]")
             continue
 
-        logging.info(f"Downloading data from panel=[{panel_name}]")
+        logging.info(f"panel=[{panel_name}]: downloading...")
         process_elements(firefox_driver)
 
     firefox_driver.quit()
 
 
 if __name__ == '__main__':
-    # Stellites dashboard catalog: https://dashboard.satnogs.org/d/QjDe5S8mk/satellite-telemetries?orgId=1
+    # Satellites dashboard catalog: https://dashboard.satnogs.org/d/QjDe5S8mk/satellite-telemetries?orgId=1
     urls: List[str] = [
         'https://dashboard.satnogs.org/d/TauG79dWz/grifex?orgId=1&from=now-9y&to=now']
     download_dir: str = './data/'
