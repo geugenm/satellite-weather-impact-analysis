814d9f33a786e057dc8b6f96bebcde7850434a5a
e_gleba <i@egleba.ru>
2024-11-20 23:47:39 +0300

    optimized js panel downloader

--- test/data_options.js
+++ test/data_options.js
@@ -1,13 +1,22 @@
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+async function wait_for_element(xpath) {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                clearInterval(interval);
+                resolve(element);
+            }
+        }, 10); // Check every 10 milliseconds
 
-async function demoSleep() {
-    await sleep(10);
+        setTimeout(() => {
+            clearInterval(interval);
+            reject(new Error("Element not found within timeout"));
+        }, 10000); // 10 seconds timeout
+    });
 }
 
-
-function clickElementByXPath(xpath) {
-    var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
+function click_element_by_xpath(xpath) {
+    const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     if (element) {
         element.click();
     } else {
@@ -15,77 +24,34 @@ function clickElementByXPath(xpath) {
     }
 }
 
-var data_options_xpath = "/html/body/div/div/div[2]/div/div[2]/div[1]/div[1]/div/div/div/div";
-clickElementByXPath(data_options_xpath);
-
-demoSleep();
-
-var xpath = '//*[starts-with(@id, "react-select-") and substring(@id, string-length(@id) - string-length("-input") + 1) = "-input"]';
-var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-if (element) {
-    var focusEvent = new FocusEvent('focus', {
-        bubbles: true,
-        cancelable: true,
-    });
-
-    var mouseDownEvent = new MouseEvent('mousedown', {
-        bubbles: true,
-        cancelable: true,
-        view: window
-    });
-
-    element.dispatchEvent(focusEvent);
-    element.dispatchEvent(mouseDownEvent);
-
-    var inputEvent = new Event('input', {
-        bubbles: true,
-        cancelable: true
-    });
-
-    element.dispatchEvent(inputEvent);
-
-    console.log("Triggered events on the input element successfully.");
-} else {
-    console.error("Element not found.");
+function dispatch_events(element) {
+    if (element) {
+        const events = ['focus', 'mousedown', 'input'];
+        events.forEach(event_type => {
+            const event = new Event(event_type, { bubbles: true, cancelable: true });
+            element.dispatchEvent(event);
+        });
+        console.log("Triggered events on the input element successfully.");
+    } else {
+        console.error("Element not found.");
+    }
 }
 
-demoSleep();
+async function main() {
+    const data_options_xpath = "/html/body/div/div/div[2]/div/div[2]/div[1]/div[1]/div/div/div/div";
+    await wait_for_element(data_options_xpath).then(() => click_element_by_xpath(data_options_xpath));
 
-var xpath2 = '//*[starts-with(@id, "react-select-") and substring(@id, string-length(@id) - string-length("-option-0") + 1) = "-option-0"]';
-// Select the input element using its ID or any other selector
-var element2 = document.evaluate(xpath2, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    const input_xpath = '//*[starts-with(@id, "react-select-") and substring(@id, string-length(@id) - string-length("-input") + 1) = "-input"]';
+    const input_element = await wait_for_element(input_xpath);
+    dispatch_events(input_element);
 
-if (element2) {
-    var focusEvent = new FocusEvent('focus', {
-        bubbles: true,
-        cancelable: true,
-    });
+    const option_xpath = '//*[starts-with(@id, "react-select-") and substring(@id, string-length(@id) - string-length("-option-0") + 1) = "-option-0"]';
+    const option_element = await wait_for_element(option_xpath);
+    dispatch_events(option_element);
+    option_element?.click();
 
-    var mouseDownEvent = new MouseEvent('mousedown', {
-        bubbles: true,
-        cancelable: true,
-        view: window
-    });
-
-    element2.dispatchEvent(focusEvent);
-    element2.dispatchEvent(mouseDownEvent);
-
-    var inputEvent = new Event('input', {
-        bubbles: true,
-        cancelable: true
-    });
-
-    element2.dispatchEvent(inputEvent);
-
-    element2.click()
-
-    console.log("Triggered events on the input element successfully.");
-} else {
-    console.error("Element not found.");
+    const download_button_xpath = "/html/body/div/div/div[2]/div/div[2]/div[1]/div[1]/button";
+    await wait_for_element(download_button_xpath).then(() => click_element_by_xpath(download_button_xpath));
 }
 
-demoSleep();
-
-var download_button_path = "/html/body/div/div/div[2]/div/div[2]/div[1]/div[1]/button";
-clickElementByXPath(download_button_path);
+main().catch(error => console.error(error.message));
