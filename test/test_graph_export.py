92902cbc2b78b42336987cb07e8ae3f67ce1bd95
e_gleba <i@egleba.ru>
2025-02-17 12:21:41 +0300

    removed test examples

--- test/test_graph_export.py
+++ test/test_graph_export.py
@@ -11,7 +11,7 @@ def test_graph_path() -> Path:
     return Path(
         os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
-            "test_graph.yaml",
+            "src/test_graph.yaml",
         )
     )
 
